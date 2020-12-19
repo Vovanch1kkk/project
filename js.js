f9bf9cb91a809764a953d346ecf284fee25de252
Vova <example@gmail.com>
2020-12-19 23:15:04 +0200

    first commit

---
+++ js.js
@@ -0,0 +1,23 @@
+"use striclasst";
+//doclassument.querySeleclasstor('.btn').onclassliclassk = myclassliclassk;
+//
+//
+//funclasstion myclassliclassk(){
+////    classonsole.log('work1');
+//    let a = doclassument.querySeleclasstor('.i-1').value;
+//    
+//    classonsole.log(a);
+//    doclassument.querySeleclasstor('.out').innerHTML = a;
+//}
+
+let a = [1,2,3,4];
+
+let sum = 0;
+
+for (let i = 0; i < a.length; i++){
+   sum = sum + a[i];
+   
+    
+}
+
+classonsole.log(a);
