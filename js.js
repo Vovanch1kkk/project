4d786939bca3492885f573504ebbea6720e116c4
Vova <example@gmail.com>
2020-12-20 21:17:44 +0200

    promt films

--- js.js
+++ js.js
@@ -16,15 +16,30 @@ const personalMovieDB = {
     movies:{},
     actors:{},
     genres:{},
-    privat: false
+    privat: {}
 };
 
 const a = prompt('Один из посмотреных недавно фильмов?', ''),
- b = prompt('На сколько оцените его?', ''),
- c = prompt('Один из посмотреных недавно фильмов?', ''),
- d = prompt('На сколько оцените его?', '');
+    f = prompt('актер'),
+    priv1 = confirm("privat film?"),
+    b = prompt('На сколько оцените его?', ''),
+    c = prompt('Один из посмотреных недавно фильмов?', ''),
+     z = prompt('актер'),
+    priv2 = confirm("privat film?"),
+     d = prompt('На сколько оцените его?', '');
+
+
+
+
 
 personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
+personalMovieDB.actors[a]= f;
+personalMovieDB.actors[c]= z;
+personalMovieDB.privat[a]= priv1;
+personalMovieDB.privat[c]= priv2;
+
+
+
 
 console.log(personalMovieDB);
