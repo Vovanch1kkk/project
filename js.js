e7bc90365007c3c842247c2dde252ebe7c3f2b16
Vova <example@gmail.com>
2020-12-21 22:05:07 +0200

    uslovia

--- js.js
+++ js.js
@@ -10,36 +10,47 @@
 //    doclassument.querySeleclasstor('.out').innerHTML = a;
 //}
 
-const numberOfflms = +prompt('Сколько фильмов вы просмотрели?', '');
-const personalMovieDB = {
-    count: numberOfflms,
-    movies:{},
-    actors:{},
-    genres:{},
-    privat: {}
-};
+// const numberOfflms = +prompt('Сколько фильмов вы просмотрели?', '');
+// const personalMovieDB = {
+//     count: numberOfflms,
+//     movies:{},
+//     actors:{},
+//     genres:{},
+//     privat: {}
+// };
+//
+// const a = prompt('Один из посмотреных недавно фильмов?', ''),
+//     f = prompt('актер'),
+//     priv1 = confirm("privat film?"),
+//     b = prompt('На сколько оцените его?', ''),
+//     c = prompt('Один из посмотреных недавно фильмов?', ''),
+//      z = prompt('актер'),
+//     priv2 = confirm("privat film?"),
+//      d = prompt('На сколько оцените его?', '');
+//
+//
+//
+//
+//
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+// personalMovieDB.actors[a]= f;
+// personalMovieDB.actors[c]= z;
+// personalMovieDB.privat[a]= priv1;
+// personalMovieDB.privat[c]= priv2;
+//
+//
+//
+//
+// console.log(personalMovieDB);
 
-const a = prompt('Один из посмотреных недавно фильмов?', ''),
-    f = prompt('актер'),
-    priv1 = confirm("privat film?"),
-    b = prompt('На сколько оцените его?', ''),
-    c = prompt('Один из посмотреных недавно фильмов?', ''),
-     z = prompt('актер'),
-    priv2 = confirm("privat film?"),
-     d = prompt('На сколько оцените его?', '');
-
-
-
-
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-personalMovieDB.actors[a]= f;
-personalMovieDB.actors[c]= z;
-personalMovieDB.privat[a]= priv1;
-personalMovieDB.privat[c]= priv2;
-
-
-
-
-console.log(personalMovieDB);
+// switch ('Vova') {
+//     case 'Vasia':
+//         console.log('NEA');
+//         break;
+//     case 'Vova':
+//         console.log('I');
+//         break;
+//     default: console.log('nety vovu');
+//         break;
+// }
