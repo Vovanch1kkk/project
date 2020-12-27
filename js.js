617fd68f2ce758739d80503e07234a2e1cb2000f
Vova <example@gmail.com>
2020-12-28 00:41:58 +0200

    28.12.2020

--- js.js
+++ js.js
@@ -10,14 +10,14 @@
 //    doclassument.querySeleclasstor('.out').innerHTML = a;
 //}
 
-const numberOfflms = +prompt('Сколько фильмов вы просмотрели?', '');
-const personalMovieDB = {
-    count: numberOfflms,
-    movies:{},
-    actors:{},
-    genres:{},
-    privat: {}
-};
+// const numberOfflms = +prompt('Сколько фильмов вы просмотрели?', '');
+// const personalMovieDB = {
+//     count: numberOfflms,
+//     movies:{},
+//     actors:{},
+//     genres:{},
+//     privat: {}
+// };
 //
 // const a = prompt('Один из посмотреных недавно фильмов?', ''),
 //     f = prompt('актер'),
@@ -77,28 +77,121 @@ const personalMovieDB = {
 //     num++;
 // }
 
-for( let i = 1; i<2; i++){
+// for( let i = 1; i<2; i++){
+//
+//
+//         a = prompt('Один из посмотреных недавно фильмов?', ''),
+//         b = prompt('На сколько оцените его?', '');
+//
+//
+//     if(a != null && b != null && a != ' ' && b != ' ' && a.length < 50){
+//
+//         personalMovieDB.movies[a] = b;
+//         console.log(personalMovieDB);
+//
+//     }
+//
+//     else{
+//         console.log('error');
+//         a = prompt('Один из посмотреных недавно фильмов?', ''),
+//             b = prompt('На сколько оцените его?', '');
+//         i--;
+//     }
+//
+//
+//
+//
+// }
 
+// let i = 0;
+//
+// while (i<2){
+//             a = prompt('Один из посмотреных недавно фильмов?', ''),
+//             b = prompt('На сколько оцените его?', '');
+//
+//             if(a != null && b != null && a != ' ' && b != ' ' && a.length < 50){
+//                 personalMovieDB.movies[a] = b;
+//                 i++;
+//             }else {
+//                 console.log('error');
+//                 a = prompt('Один из посмотреных недавно фильмов?', ''),
+//                     b = prompt('На сколько оцените его?', '');
+//                 i--;
+//             }
+//
+//
+// }
+//
+// console.log(personalMovieDB);
+// let i = 0;
+// do{
+//         a = prompt('Один из посмотреных недавно фильмов?', ''),
+//         b = prompt('На сколько оцените его?', '');
+//     if(a != null && b != null && a != ' ' && b != ' ' && a.length < 50){
+//         personalMovieDB.movies[a] = b;
+//         i++;
+//     }else {
+//         console.log('error');
+//         a = prompt('Один из посмотреных недавно фильмов?', ''),
+//             b = prompt('На сколько оцените его?', '');
+//         i--;
+//     }
+//
+// }
+//
+// while (i < 2);
+//
+// console.log(personalMovieDB);
 
-        a = prompt('Один из посмотреных недавно фильмов?', ''),
-        b = prompt('На сколько оцените его?', '');
+// let i = 50;
+// function linckName(vova) {
+//     console.log(vova);
+//      i = 4;
+// }
+//
+// linckName('kurva');
+// console.log(i);
+//
+// function calc(a, b) {
+//   return (a+b);
+// }
+//
+// console.log(calc(49,6));
 
+// let calc = function (a,b) {
+//     return(a+b);
+//
+//
+// };
+//
+// console.log(calc(4,6));
 
-    if(a != null && b != null && a != ' ' && b != ' ' && a.length < 50){
+// let name = (kat, vova) => {let rezult = `${kat}    ${1} ${vova}`; return rezult;}
+//
+// console.log(name('vova','kat'));
 
-        personalMovieDB.movies[a] = b;
-        console.log(personalMovieDB);
+let str = 'voKKKal kakax';
+let arr = [1,2,3];
 
-    }
+// console.log(arr.length);
 
-    else{
-        console.log('error');
-        a = prompt('Один из посмотреных недавно фильмов?', ''),
-            b = prompt('На сколько оцените его?', '');
-        i--;
-    }
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
 
+// console.log(str.indexOf('kakax'));
 
+// console.log(str.slice(8,13));
 
+// console.log(str.substring(1,3));
 
-}
+// console.log(str.substr(8,13));
+
+// const num = 12.5;
+//
+// console.log(Math.round(num));
+
+const pixl = '13.6px';
+
+// console.log(parseInt(pixl));
+
+console.log(parseFloat(pixl));
